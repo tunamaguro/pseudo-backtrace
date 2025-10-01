0ebc5221fc007011eebf42d3f8dcd1b83d44ce91
tunamaguro <contact@tunamaguro.dev>
2025-10-01 11:48:31 +0000

    fix: correct chain writer impl

--- src/lib.rs
+++ src/lib.rs
@@ -22,11 +22,6 @@ impl<'a> Chain<'a> {
         }
     }
 
-    /// Into the iterator
-    pub const fn into_iter(self) -> Iter<'a> {
-        Iter { stack: Some(self) }
-    }
-
     /// Returns the underlying error for this stack layer.
     pub const fn inner(&'a self) -> &'a dyn core::error::Error {
         match self {
@@ -58,6 +53,15 @@ impl core::fmt::Display for Chain<'_> {
 impl core::error::Error for Chain<'_> {
     fn source(&self) -> Option<&(dyn core::error::Error + 'static)> {
         self.inner().source()
+    }
+}
+
+impl<'a> IntoIterator for Chain<'a> {
+    type Item = Chain<'a>;
+    type IntoIter = Iter<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Iter { stack: Some(self) }
     }
 }
 
@@ -192,12 +196,15 @@ pub struct ChainWriter<'a> {
 
 impl<'a> core::fmt::Display for ChainWriter<'a> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        let it = self.stack.clone().into_iter();
-        let std_count = 0;
-        for (i, err) in it
-            .enumerate()
-            .take_while(move |_| std_count < self.std_limit)
-        {
+        let mut std_remaining = self.std_limit;
+        for (i, err) in self.stack.clone().into_iter().enumerate() {
+            if matches!(err, Chain::Std(_)) {
+                if std_remaining == 0 {
+                    break;
+                }
+                std_remaining -= 1;
+            }
+
             writeln!(f, "{}: {}", i, err)?;
         }
 
@@ -236,7 +243,7 @@ pub trait StackErrorExt: StackError + Sized {
     /// // 3: StdError A
     /// // 4: StdError B
     /// // 5: StdError C
-    /// 
+    ///
     /// println!("{}", err.to_chain_with_limit(0));
     /// // 0: StackError A, at src/main.rs:20:5
     /// // 1: StackError B, at src/main.rs:19:5
@@ -312,3 +319,132 @@ pub trait StackErrorExt: StackError + Sized {
 }
 
 impl<E: StackError> StackErrorExt for E {}
+
+#[cfg(test)]
+mod tests {
+    extern crate std;
+
+    use core::usize;
+
+    use super::{Chain, StackError, StackErrorExt};
+    #[derive(Debug)]
+    struct NestedStd {
+        source: std::boxed::Box<dyn core::error::Error + 'static>,
+    }
+
+    impl NestedStd {
+        fn new<E>(source: E) -> Self
+        where
+            E: core::error::Error + 'static,
+        {
+            Self {
+                source: std::boxed::Box::new(source),
+            }
+        }
+
+        fn nest(self) -> Self {
+            Self {
+                source: std::boxed::Box::new(self),
+            }
+        }
+    }
+
+    impl core::fmt::Display for NestedStd {
+        fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+            self.source.fmt(f)
+        }
+    }
+
+    impl core::error::Error for NestedStd {
+        fn source(&self) -> Option<&(dyn core::error::Error + 'static)> {
+            Some(self.source.as_ref())
+        }
+    }
+
+    #[derive(Debug)]
+    enum Stacked {
+        Stacked {
+            source: std::boxed::Box<Stacked>,
+            location: &'static core::panic::Location<'static>,
+        },
+        Std {
+            source: std::boxed::Box<dyn core::error::Error + 'static>,
+            location: &'static core::panic::Location<'static>,
+        },
+    }
+
+    impl Stacked {
+        fn new<E>(source: E) -> Self
+        where
+            E: core::error::Error + 'static,
+        {
+            Stacked::Std {
+                source: std::boxed::Box::new(source),
+                location: core::panic::Location::caller(),
+            }
+        }
+
+        #[track_caller]
+        fn stack(self) -> Self {
+            Self::Stacked {
+                source: std::boxed::Box::new(self),
+                location: core::panic::Location::caller(),
+            }
+        }
+    }
+
+    impl core::fmt::Display for Stacked {
+        fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+            match self {
+                Stacked::Stacked { .. } => "Stacked".fmt(f),
+                Stacked::Std { .. } => "Std".fmt(f),
+            }
+        }
+    }
+
+    impl core::error::Error for Stacked {
+        fn source(&self) -> Option<&(dyn core::error::Error + 'static)> {
+            match self {
+                Stacked::Stacked { source, .. } => Some(source),
+                Stacked::Std { source, .. } => Some(source.as_ref()),
+            }
+        }
+    }
+
+    impl StackError for Stacked {
+        fn location(&self) -> &'static core::panic::Location<'static> {
+            match self {
+                Stacked::Stacked { location, .. } => location,
+                Stacked::Std { location, .. } => location,
+            }
+        }
+
+        fn next<'a>(&'a self) -> Option<crate::Chain<'a>> {
+            match self {
+                Stacked::Stacked { source, .. } => Some(Chain::Stacked(source.as_ref())),
+                Stacked::Std { source, .. } => Some(Chain::Std(source.as_ref())),
+            }
+        }
+    }
+
+    #[test]
+    fn stack_writer_limit() {
+        use std::string::ToString;
+        let a = std::io::Error::other("Error A");
+        let b = NestedStd::new(a);
+        let c = b.nest();
+        let d = Stacked::new(c);
+        let e = d.stack();
+        let f = e.stack();
+        let g = f.stack();
+
+        assert_eq!(g.iter().count(), 7);
+
+        let stack = g.to_chain().to_string();
+        assert_eq!(stack.lines().count(), 5);
+
+        let stack = g.to_chain_with_limit(usize::MAX).to_string();
+        std::println!("to_chain_with_limit: {stack}");
+        assert_eq!(stack.lines().count(), 7);
+    }
+}
