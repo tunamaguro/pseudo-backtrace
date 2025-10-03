d1ce28d1a336c231e401e03beea44b09315ac4b8
tunamaguro <contact@tunamaguro.dev>
2025-10-03 14:34:09 +0000

    feat: collect bound

--- pseudo-backtrace-derive/src/expand.rs
+++ pseudo-backtrace-derive/src/expand.rs
@@ -52,7 +52,8 @@ impl Struct<'_> {
         let body = if let Some(source) = source {
             let member = source.member.clone();
             let kind = source.stack_error_kind();
-            match (is_option_type(&source.ty), kind) {
+            let is_option = option_inner_type(&source.ty).is_some();
+            match (is_option, kind) {
                 (true, StackErrorKind::Stacked) => {
                     quote! {
                         self.#member
@@ -101,7 +102,12 @@ fn expand_struct(input: Struct<'_>) -> Result<proc_macro2::TokenStream> {
     let next_fn = input.next_fn()?;
 
     let ident = input.ident.clone();
+
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+    let mut tracker = BoundsTracker::new(input.generics);
+    tracker.collect(&input.fields);
+    let where_clause = tracker.make_where_clause(where_clause);
+
     Ok(quote! {
         impl #impl_generics ::pseudo_backtrace::StackError for #ident #ty_generics #where_clause {
            #location_fn
@@ -191,7 +197,9 @@ impl Variant<'_> {
 
         // Build the body depending on Option<T> and stack_error kind
         let kind = source.stack_error_kind();
-        let body = match (is_option_type(&source.ty), kind) {
+        let is_option = option_inner_type(&source.ty).is_some();
+
+        let body = match (is_option, kind) {
             (true, StackErrorKind::Stacked) => {
                 quote! {
                     #binding
@@ -239,7 +247,7 @@ fn expand_enum(input: Enum<'_>) -> Result<proc_macro2::TokenStream> {
         .map(|v| v.next_body())
         .collect::<Result<Vec<_>>>()?;
 
-    let ident = input.ident;
+    let ident = input.ident.clone();
     let location_fn = quote! {
         fn location(&self) -> &'static ::core::panic::Location<'static> {
             use #ident::*;
@@ -260,6 +268,10 @@ fn expand_enum(input: Enum<'_>) -> Result<proc_macro2::TokenStream> {
     };
 
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+    let mut tracker = BoundsTracker::new(input.generics);
+    tracker.collect_enum(&input);
+    let where_clause = tracker.make_where_clause(where_clause);
+
     Ok(quote! {
         impl #impl_generics ::pseudo_backtrace::StackError for #ident #ty_generics #where_clause {
            #location_fn
@@ -366,13 +378,114 @@ fn is_located_error(ty: &syn::Type) -> bool {
     last.ident == "LocatedError"
 }
 
-fn is_option_type(ty: &syn::Type) -> bool {
+fn option_inner_type(ty: &syn::Type) -> Option<&syn::Type> {
     let path = match ty {
         syn::Type::Path(ty) => &ty.path,
-        _ => return false,
+        _ => return None,
     };
-    match path.segments.last() {
-        Some(last) => last.ident == "Option",
-        None => false,
+
+    let last = path.segments.last()?;
+    if last.ident != "Option" {
+        return None;
+    };
+
+    let args = match &last.arguments {
+        syn::PathArguments::AngleBracketed(args) => args,
+        _ => {
+            return None;
+        }
+    };
+
+    let first = args.args.first()?;
+
+    match first {
+        syn::GenericArgument::Type(t) => Some(t),
+        _ => None,
+    }
+}
+
+pub struct BoundsTracker {
+    params: std::collections::BTreeSet<syn::Ident>,
+    stack_bounds: std::collections::BTreeMap<String, Vec<StackErrorKind>>,
+}
+
+impl BoundsTracker {
+    pub fn new(generics: &syn::Generics) -> Self {
+        let params = generics.type_params().map(|p| p.ident.clone()).collect();
+        Self {
+            params,
+            stack_bounds: Default::default(),
+        }
+    }
+
+    pub fn collect(&mut self, fields: &[Field<'_>]) {
+        use quote::ToTokens;
+        let Ok(Some(f)) = find_source(fields) else {
+            return;
+        };
+
+        let mut found = false;
+        crawl(&f.ty, &self.params, &mut found);
+
+        if found {
+            self.stack_bounds
+                .entry(f.ty.to_token_stream().to_string())
+                .or_default()
+                .push(f.stack_error_kind());
+        }
+    }
+
+    pub fn collect_enum(&mut self, enum_st: &Enum<'_>) {
+        for variant in &enum_st.variants {
+            self.collect(&variant.fields);
+        }
+    }
+
+    pub fn make_where_clause(&self, where_clause: Option<&syn::WhereClause>) -> syn::WhereClause {
+        let mut where_clause = where_clause.cloned().unwrap_or(syn::WhereClause {
+            where_token: syn::Token![where](Span::call_site()),
+            predicates: syn::punctuated::Punctuated::new(),
+        });
+
+        for (ty, kinds) in &self.stack_bounds {
+            let ty: syn::Type = syn::parse_str(ty).unwrap();
+            for kind in kinds {
+                let bound = match kind {
+                    StackErrorKind::Stacked => quote! {::pseudo_backtrace::StackError},
+                    StackErrorKind::Std => quote! {::core::error::Error},
+                };
+                where_clause.predicates.push(syn::parse_quote!(#ty: #bound));
+            }
+        }
+
+        where_clause
+    }
+}
+
+fn crawl(ty: &syn::Type, params: &std::collections::BTreeSet<syn::Ident>, found: &mut bool) {
+    let syn::Type::Path(ty) = ty else {
+        return;
+    };
+
+    if let Some(qself) = &ty.qself {
+        crawl(&qself.ty, params, found);
+    }
+
+    if let Some(first) = ty.path.segments.first() {
+        let no_args = first.arguments.is_none();
+        let is_param = params.contains(&first.ident);
+        if no_args && is_param {
+            *found = true;
+        }
+    }
+
+    for seg in ty.path.segments.iter() {
+        if let syn::PathArguments::AngleBracketed(arguments) = &seg.arguments {
+            for arg in arguments.args.iter() {
+                if let syn::GenericArgument::Type(ty) = &arg {
+                    crawl(ty, params, found);
+                }
+            }
+        }
     }
 }
